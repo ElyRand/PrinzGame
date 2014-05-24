d86eb0b231fdb789a51e9c6b97ae9c3da9142738
elyeser <elyeser.f@gmail.com>
2014-05-24 16:36:50 -0400

    prefabs

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+	public Vector3 speed = new Vector3(1,1,1);
+	public Vector3 direction = new Vector3(0,0,0);
+
+	
+	void Update()
+	{
+		gameObject.transform.Translate (new Vector3 (speed.x * direction.x, speed.y * direction.y, speed.z * direction.z)); 
+
+	}
+	
+	void FixedUpdate()
+	{
+
+	}
+}
